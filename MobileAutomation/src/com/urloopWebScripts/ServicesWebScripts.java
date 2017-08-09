0a9a086573e9ee1ac8a147a4fdc79d843f1da560
VIJAY KUMAR <VIJAY KUMAR@Admin>
2017-08-09 04:37:49 -0700

    Commited Services Constant Changes

--- MobileAutomation/src/com/urloopWebScripts/ServicesWebScripts.java
+++ MobileAutomation/src/com/urloopWebScripts/ServicesWebScripts.java
@@ -1,6 +1,7 @@
 package com.urloopWebScripts;
 
 import java.awt.AWTException;
+import webScriptsConstants.*;
 
 import org.hamcrest.core.StringContains;
 import org.openqa.selenium.By;
@@ -9,7 +10,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
 import com.Tring.nbc.utils.CommonUtils;
-import com.Tring.nbc.utils.WebScriptConstants;
+
 import com.excelGetterSetter.ServicesGS;
 import com.urloopWebPages.Browser;
 
@@ -17,12 +18,12 @@ public class ServicesWebScripts {
 	
 	public static void slideMainMenuServiceClick() throws InterruptedException{
 		Thread.sleep(15000);
-		Browser.findElementByPath(WebScriptConstants.SERVICESMAINMENU).click();
+		Browser.findElementByPath(ServicesWebScriptsConstants.SERVICESMAINMENU).click();
 		
 		
 Thread.sleep(5000);
 		
-		WebElement v = Browser.findElementByPath(WebScriptConstants.SERVICE_BREAD_CRUMS);
+		WebElement v = Browser.findElementByPath(ServicesWebScriptsConstants.SERVICE_BREAD_CRUMS);
 		((JavascriptExecutor)Browser.driver).executeScript("arguments[0].scrollIntoView();", v);
 
 		
@@ -36,7 +37,7 @@ Thread.sleep(5000);
 		Thread.sleep(10000);
 		
 		WebElement targetElement = Browser
-				.findElementByPath(WebScriptConstants.ROOMSERVICE);
+				.findElementByPath(ServicesWebScriptsConstants.ROOMSERVICE);
 		Actions action = new Actions(Browser.driver);
 		/* action.click(targetElement); */
 		action.moveToElement(targetElement);
@@ -44,26 +45,26 @@ Thread.sleep(5000);
         
 		Thread.sleep(5000);
 		
-		Browser.findElementByPath(WebScriptConstants.ROOMSERVICEHOVER).click();
+		Browser.findElementByPath(ServicesWebScriptsConstants.ROOMSERVICEHOVER).click();
 		Thread.sleep(10000);
-		Browser.findElementByPath(WebScriptConstants.ROOMNAME).sendKeys(servicesGS.getRoomName());
+		Browser.findElementByPath(ServicesWebScriptsConstants.ROOMNAME).sendKeys(servicesGS.getRoomName());
 		Thread.sleep(5000);
 		
 		System.out.println("Room Price :"+servicesGS.getRoomRentPrice());
 		
-		Browser.findElementByPath(WebScriptConstants.ROOMRENTPRICE).sendKeys(String.valueOf(servicesGS.getRoomRentPrice()));
+		Browser.findElementByPath(ServicesWebScriptsConstants.ROOMRENTPRICE).sendKeys(String.valueOf(servicesGS.getRoomRentPrice()));
 		Thread.sleep(5000);
-		Browser.findElementByPath(WebScriptConstants.ROOMDESCRIPTION).sendKeys(servicesGS.getDecription());
+		Browser.findElementByPath(ServicesWebScriptsConstants.ROOMDESCRIPTION).sendKeys(servicesGS.getDecription());
 		System.out.println("Room Description :"+servicesGS.getDecription());
 		Thread.sleep(10000);
-		Browser.findElementByPath(WebScriptConstants.SAVERENTOPTION).click();
+		Browser.findElementByPath(ServicesWebScriptsConstants.SAVERENTOPTION).click();
 		Thread.sleep(10000);
 		
 		JavascriptExecutor jse = (JavascriptExecutor) Browser.driver;
 		
 		jse.executeScript("window.scrollBy(0,-250)", "");
 		Thread.sleep(5000);
-		Browser.findElementByPath(WebScriptConstants.SAVE).click();
+		Browser.findElementByPath(ServicesWebScriptsConstants.SAVE).click();
 
 	}
 	
@@ -74,20 +75,20 @@ Thread.sleep(5000);
 		Thread.sleep(5000);
 		
 	    WebElement targetElement = Browser
-				.findElementByPath(WebScriptConstants.BOARDINGSERVICE);
+				.findElementByPath(ServicesWebScriptsConstants.BOARDINGSERVICE);
 		Actions action = new Actions(Browser.driver);
 		action.moveToElement(targetElement);
 		action.perform();
         
 		Thread.sleep(5000);
 		
-		Browser.findElementByPath(WebScriptConstants.BOARDINGSERVICEHOVER).click();
+		Browser.findElementByPath(ServicesWebScriptsConstants.BOARDINGSERVICEHOVER).click();
 		Thread.sleep(10000);
 		
-		Browser.findElementByPath(WebScriptConstants.BOARDINGPACKAGENAME).sendKeys(servicesGS.getPackageName());
+		Browser.findElementByPath(ServicesWebScriptsConstants.BOARDINGPACKAGENAME).sendKeys(servicesGS.getPackageName());
 		Thread.sleep(5000);
 		
-		Browser.findElementByPath(WebScriptConstants.BOARDINGPACKAGEPRICE).sendKeys(servicesGS.getPackageBasePrice());
+		Browser.findElementByPath(ServicesWebScriptsConstants.BOARDINGPACKAGEPRICE).sendKeys(servicesGS.getPackageBasePrice());
 		Thread.sleep(5000);
 		
 		
@@ -97,14 +98,14 @@ Thread.sleep(5000);
 	
 		Thread.sleep(5000);
 	    WebElement bOARDINGSELECTSERVICEVALUE = Browser
-				.findElementByPath(WebScriptConstants.BOARDINGSELECTSERVICEVALUE);
+				.findElementByPath(ServicesWebScriptsConstants.BOARDINGSELECTSERVICEVALUE);
 		JavascriptExecutor bOARDINGSELECTSERVICEVALUEexecutor = (JavascriptExecutor)Browser.driver;
 		bOARDINGSELECTSERVICEVALUEexecutor.executeScript("arguments[0].click();", bOARDINGSELECTSERVICEVALUE);
 		
 		Thread.sleep(5000);
 		
 		WebElement bOARDINGSELECTQUANTITYVALUE = Browser
-				.findElementByPath(String.format(WebScriptConstants.BOARDINGSELECTQUANTITYVALUE, servicesGS.getPackageSelectQuantity()));
+				.findElementByPath(String.format(ServicesWebScriptsConstants.BOARDINGSELECTQUANTITYVALUE, servicesGS.getPackageSelectQuantity()));
 		JavascriptExecutor bOARDINGSELECTQUANTITYVALUEexecutor = (JavascriptExecutor)Browser.driver;
 		bOARDINGSELECTQUANTITYVALUEexecutor.executeScript("arguments[0].click();", bOARDINGSELECTQUANTITYVALUE);
 		
@@ -112,7 +113,7 @@ Thread.sleep(5000);
 Thread.sleep(5000);
 		
 		WebElement bOARDINGSELECTOCCURRENCEVALUE = Browser
-				.findElementByPath(String.format(WebScriptConstants.BOARDINGSELECTOCCURRENCEVALUE, servicesGS.getPackageSelectOccurrence()));
+				.findElementByPath(String.format(ServicesWebScriptsConstants.BOARDINGSELECTOCCURRENCEVALUE, servicesGS.getPackageSelectOccurrence()));
 		JavascriptExecutor bOARDINGSELECTOCCURRENCEVALUEexecutor = (JavascriptExecutor)Browser.driver;
 		bOARDINGSELECTOCCURRENCEVALUEexecutor.executeScript("arguments[0].click();", bOARDINGSELECTOCCURRENCEVALUE);
 		
@@ -120,29 +121,29 @@ Thread.sleep(5000);
 Thread.sleep(5000);
 		
 		WebElement bOARDINGGLOBALHTML = Browser
-				.findElementByPath(WebScriptConstants.BOARDINGGLOBALHTML);
+				.findElementByPath(ServicesWebScriptsConstants.BOARDINGGLOBALHTML);
 		JavascriptExecutor bOARDINGGLOBALHTMLexecutor = (JavascriptExecutor)Browser.driver;
 		bOARDINGGLOBALHTMLexecutor.executeScript("arguments[0].click();", bOARDINGGLOBALHTML);
 		
 		Thread.sleep(5000);
 		
-		Browser.findElementByPath(WebScriptConstants.BOARDINGMEALVOUCHERSFIELD).sendKeys(servicesGS.getPackageNumberOfVouchers());
+		Browser.findElementByPath(ServicesWebScriptsConstants.BOARDINGMEALVOUCHERSFIELD).sendKeys(servicesGS.getPackageNumberOfVouchers());
 		Thread.sleep(5000);
 		
-		Browser.findElementByPath(WebScriptConstants.BOARDINGMEALVOUCHERSPERDAYFIELD).sendKeys(servicesGS.getPackageNumberOfVouchersPerDay());
+		Browser.findElementByPath(ServicesWebScriptsConstants.BOARDINGMEALVOUCHERSPERDAYFIELD).sendKeys(servicesGS.getPackageNumberOfVouchersPerDay());
 		Thread.sleep(5000);
 		
-		Browser.findElementByPath(WebScriptConstants.BOARDINGMEALPRICEPERVOUCHERS).sendKeys(servicesGS.getPackagePricePerVoucher());
+		Browser.findElementByPath(ServicesWebScriptsConstants.BOARDINGMEALPRICEPERVOUCHERS).sendKeys(servicesGS.getPackagePricePerVoucher());
 		Thread.sleep(5000);
 		
 		
-		Browser.findElementByPath(WebScriptConstants.BOARDINGSAVEPACKAGE).click();
+		Browser.findElementByPath(ServicesWebScriptsConstants.BOARDINGSAVEPACKAGE).click();
 		Thread.sleep(5000);
 		
 		JavascriptExecutor jsec = (JavascriptExecutor) Browser.driver;
 		jsec.executeScript("window.scrollBy(0,-250)", "");
 		Thread.sleep(5000);
-		Browser.findElementByPath(WebScriptConstants.BOARDINGGLOBALSAVE).click();
+		Browser.findElementByPath(ServicesWebScriptsConstants.BOARDINGGLOBALSAVE).click();
 	
 	}
 	
@@ -151,12 +152,12 @@ Thread.sleep(5000);
 		slideMainMenuServiceClick();
 		
 		Thread.sleep(10000);
-		Browser.findElementByPath(WebScriptConstants.CREATE_A_NEW_SERVICE).click();
+		Browser.findElementByPath(ServicesWebScriptsConstants.CREATE_A_NEW_SERVICE).click();
 		
 		
 Thread.sleep(10000);
 		
-		WebElement v = Browser.findElementByPath(WebScriptConstants.SERVICE_BREAD_CRUMS);
+		WebElement v = Browser.findElementByPath(ServicesWebScriptsConstants.SERVICE_BREAD_CRUMS);
 		((JavascriptExecutor)Browser.driver).executeScript("arguments[0].scrollIntoView();", v);
 
 		
@@ -164,81 +165,81 @@ Thread.sleep(10000);
 		CommonUtils.attachment(".//*[@id='fileinfo']/a[text()='Add Image']","Group.jpg");
 		
 		Thread.sleep(5000);
-		Browser.findElementByPath(WebScriptConstants.SERVICENAME).sendKeys(servicesGS.getServiceName());
+		Browser.findElementByPath(ServicesWebScriptsConstants.SERVICENAME).sendKeys(servicesGS.getServiceName());
 		
 		Thread.sleep(5000);
-		Browser.findElementByPath(WebScriptConstants.SERVICECATEGORYFIELD).click();
+		Browser.findElementByPath(ServicesWebScriptsConstants.SERVICECATEGORYFIELD).click();
 		
 		
 		Thread.sleep(5000);
-		Browser.findElementByPath(String.format(WebScriptConstants.SERVICECATEGORYSELECTION,servicesGS.getServiceCategory())).click();
+		Browser.findElementByPath(String.format(ServicesWebScriptsConstants.SERVICECATEGORYSELECTION,servicesGS.getServiceCategory())).click();
 		
 		Thread.sleep(5000);
-		Browser.findElementByPath(WebScriptConstants.SERVICEDURATION).sendKeys(servicesGS.getServiceTaskDuration());
+		Browser.findElementByPath(ServicesWebScriptsConstants.SERVICEDURATION).sendKeys(servicesGS.getServiceTaskDuration());
 		
 		Thread.sleep(5000);
-		Browser.findElementByPath(WebScriptConstants.SERVICEMINUTESORHOURSFIELD).click();
+		Browser.findElementByPath(ServicesWebScriptsConstants.SERVICEMINUTESORHOURSFIELD).click();
 		
 		
 		if(servicesGS.getServiceMinutesOrHours().trim().equals("Minutes")){
 			Thread.sleep(5000);
-			Browser.findElementByPath(WebScriptConstants.SERVICEMINUTESORHOURSSELECTIONMINUTES).click();
+			Browser.findElementByPath(ServicesWebScriptsConstants.SERVICEMINUTESORHOURSSELECTIONMINUTES).click();
 		}
 		
 		else if(servicesGS.getServiceMinutesOrHours().trim().equals("Hours")){
 			Thread.sleep(5000);
-			Browser.findElementByPath(WebScriptConstants.SERVICEMINUTESORHOURSSELECTIONHOURS).click();
+			Browser.findElementByPath(ServicesWebScriptsConstants.SERVICEMINUTESORHOURSSELECTIONHOURS).click();
 		}
 		else{}
 		
 		Thread.sleep(5000);
-		Browser.findElementByPath(WebScriptConstants.SERVICESERVICETEAMFIELD).click();
+		Browser.findElementByPath(ServicesWebScriptsConstants.SERVICESERVICETEAMFIELD).click();
 		
 		Thread.sleep(5000);
-		Browser.findElementByPath(String.format(WebScriptConstants.SERVICESERVICETEAM,servicesGS.getServiceServiceTeam())).click();
+		Browser.findElementByPath(String.format(ServicesWebScriptsConstants.SERVICESERVICETEAM,servicesGS.getServiceServiceTeam())).click();
 
 		Thread.sleep(5000);
-		Browser.findElementByPath(WebScriptConstants.SERVICEDESCRIPTION).sendKeys(servicesGS.getServiceDescription());
+		Browser.findElementByPath(ServicesWebScriptsConstants.SERVICEDESCRIPTION).sendKeys(servicesGS.getServiceDescription());
 		
 		if(Boolean.valueOf(servicesGS.getServiceOnDemandServiceBoolean())){
 			
 			Thread.sleep(5000);
-			Browser.findElementByPath(WebScriptConstants.SERVICEONDEMANDBOOLEAN).click();
+			Browser.findElementByPath(ServicesWebScriptsConstants.SERVICEONDEMANDBOOLEAN).click();
 			
 			Thread.sleep(5000);
-			Browser.findElementByPath(WebScriptConstants.SERVICE_ONDEMAND_BOOLEANBASECOST_PRICE).sendKeys(servicesGS.getServiceOnDemandServiceBoolean_baseCostPrice());
+			Browser.findElementByPath(ServicesWebScriptsConstants.SERVICE_ONDEMAND_BOOLEANBASECOST_PRICE).sendKeys(servicesGS.getServiceOnDemandServiceBoolean_baseCostPrice());
 			
 			Thread.sleep(5000);
-			Browser.findElementByPath(WebScriptConstants.SERVICE_ONDEMAND_BOOLEAN_BASEBILLABLE_PRICE).sendKeys(servicesGS.getServiceOnDemandServiceBoolean_basebillablePrice());
+			Browser.findElementByPath(ServicesWebScriptsConstants.SERVICE_ONDEMAND_BOOLEAN_BASEBILLABLE_PRICE).sendKeys(servicesGS.getServiceOnDemandServiceBoolean_basebillablePrice());
 			}
 		else{}
 		
 		Thread.sleep(5000);
-		Browser.findElementByPath(WebScriptConstants.SERVICE_DAILY_BASE_COST_PRICE).sendKeys(servicesGS.getServiceDailyCostPrice());
+		Browser.findElementByPath(ServicesWebScriptsConstants.SERVICE_DAILY_BASE_COST_PRICE).sendKeys(servicesGS.getServiceDailyCostPrice());
 		
 		Thread.sleep(5000);
-		Browser.findElementByPath(WebScriptConstants.SERVICE_DAILY_BASE_BILLABLE_PRICE).sendKeys(servicesGS.getServiceDailyBillablePrice());
+		Browser.findElementByPath(ServicesWebScriptsConstants.SERVICE_DAILY_BASE_BILLABLE_PRICE).sendKeys(servicesGS.getServiceDailyBillablePrice());
 
 		Thread.sleep(5000);
-		Browser.findElementByPath(WebScriptConstants.SERVICE_WEEKLY_BASE_COST_PRICE).sendKeys(servicesGS.getServiceWeeklyCostPrice());
+		Browser.findElementByPath(ServicesWebScriptsConstants.SERVICE_WEEKLY_BASE_COST_PRICE).sendKeys(servicesGS.getServiceWeeklyCostPrice());
 		
 		Thread.sleep(5000);
-		Browser.findElementByPath(WebScriptConstants.SERVICE_WEEKLY_BASE_BILLABLE_PRICE).sendKeys(servicesGS.getServiceWeeklyBillablePrice());
+		Browser.findElementByPath(ServicesWebScriptsConstants.SERVICE_WEEKLY_BASE_BILLABLE_PRICE).sendKeys(servicesGS.getServiceWeeklyBillablePrice());
 		
 		Thread.sleep(5000);
-		Browser.findElementByPath(WebScriptConstants.SERVICE_MONTH_BASE_COST_PRICE).sendKeys(servicesGS.getServiceMonthlyCostPrice());
+		Browser.findElementByPath(ServicesWebScriptsConstants.SERVICE_MONTH_BASE_COST_PRICE).sendKeys(servicesGS.getServiceMonthlyCostPrice());
 		
 		Thread.sleep(5000);
-		Browser.findElementByPath(WebScriptConstants.SERVICE_MONTH_BASE_BILLABLE_PRICE).sendKeys(servicesGS.getServiceMonthlyBillablePrice());
+		Browser.findElementByPath(ServicesWebScriptsConstants.SERVICE_MONTH_BASE_BILLABLE_PRICE).sendKeys(servicesGS.getServiceMonthlyBillablePrice());
 
 		
 Thread.sleep(5000);
 		
-		WebElement c = Browser.findElementByPath(WebScriptConstants.SERVICE_BREAD_CRUMS);
+		WebElement c = Browser.findElementByPath(ServicesWebScriptsConstants.SERVICE_BREAD_CRUMS);
 		((JavascriptExecutor)Browser.driver).executeScript("arguments[0].scrollIntoView();", c);
 		
 		Thread.sleep(5000);
-		Browser.findElementByPath(WebScriptConstants.SAVE).click();
+		Browser.findElementByPath(ServicesWebScriptsConstants.SAVE).click();
 		}
 	
 
--- MobileAutomation/src/webScriptsConstants/ServicesWebScriptsConstants.java
+++ MobileAutomation/src/webScriptsConstants/ServicesWebScriptsConstants.java
@@ -1,5 +1,70 @@
 package webScriptsConstants;
 
 public interface ServicesWebScriptsConstants {
+	
+	
+/*Service */
+	
+	String SERVICESMAINMENU = ".//*[@id='side-menu']/li[4]/a/span[text()='Services']";
+	String CREATE_A_NEW_SERVICE = ".//*[@id='btn-create']";
+	
+	
+	/*ROOM SERVICES*/
+	String ROOMSERVICE = ".//*[@id='services']/div[2]/div/div/div[2]/div/div[1]/a/div";
+	String ROOMSERVICEHOVER = ".//*[@id='services']/div[2]/div/div/div[2]/div/div[1]/a/div/div/div[text()='Edit Service']";
+	String ROOMNAME = ".//*[@id='roomName']";
+	String ROOMRENTPRICE = ".//*[@id='price']";
+	String ROOMDESCRIPTION = ".//*[@id='description']";
+	String SAVERENTOPTION = ".//*[@id='rentalformsave'][text()='Save Rent Option']";
+	String SAVE = ".//*[@id='btn-save'][text()='Save']";
+	/*ROOM SERVICES*/
+	
+	/*Boarding Services*/
+	
+	String BOARDINGSERVICE = ".//*[@id='services']/div[2]/div/div/div[2]/div/div[2]/a/div";
+	String BOARDINGSERVICEHOVER = ".//*[@id='services']/div[2]/div/div/div[2]/div/div[2]/a/div/div/div[text()='Edit Service']";
+	String BOARDINGPACKAGENAME =".//*[@id='packageName']";
+	String BOARDINGPACKAGEPRICE =".//*[@id='packageprice']";
+	String BOARDINGSELECTSERVICEFIELD =".//*[@aria-label='Select Service*']";
+	/*HardCoded*/
+	String BOARDINGSELECTSERVICEVALUE =".//*[@value=170]";
+	/*HardCoded*/
+	String BOARDINGSELECTQUANTITYFIELD =".//*[@aria-label='Quantity*']";
+	String BOARDINGSELECTQUANTITYVALUE =".//*[@class='md-text ng-binding'][text()=%s]";
+	String BOARDINGSELECTOCCURRENCEFIELD =".//*[@id='select_value_label_2']";
+	String BOARDINGSELECTOCCURRENCEVALUE = ".//*[@value='%s']/div";
+	String BOARDINGMEALVOUCHERSFIELD =".//*[@id='mealVouchers']";
+	String BOARDINGMEALVOUCHERSPERDAYFIELD =".//*[@id='voucherPerDay']";
+	String BOARDINGMEALPRICEPERVOUCHERS =".//*[@id='pricePerVoucher']";
+	String BOARDINGSAVEPACKAGE =".//*[@id='packageformsave']";
+	String BOARDINGGLOBALSAVE =".//*[@id='btn-save']";
+	String BOARDINGGLOBALHTML ="html/body/md-backdrop";
+	/*Boarding Services*/
+	
+	/*Service Creation*/
+	String SERVICENAME = ".//*[@id='serviceName']";
+	String SERVICECATEGORYFIELD = ".//*[@id='categoryId']";
+	String SERVICECATEGORYSELECTION = ".//*[@id='select-options-categoryId']/ul/li[text()='%s']";
+	String SERVICEDURATION = ".//*[@id='taskDuration']";
+	String SERVICEMINUTESORHOURSFIELD = ".//*[@id='taskDurationUnit']";
+	String SERVICEMINUTESORHOURSSELECTIONMINUTES = ".//*[@id='select-options-taskDurationUnit']/ul/li[1]";
+	String SERVICEMINUTESORHOURSSELECTIONHOURS = ".//*[@id='select-options-taskDurationUnit']/ul/li[2]";
+	String SERVICESERVICETEAMFIELD = ".//*[@id='executionTeamId']";		
+	String SERVICESERVICETEAM = ".//*[@value='%s']";
+	String SERVICEDESCRIPTION= ".//*[@id='description']";
+	String SERVICEONDEMANDBOOLEAN= ".//*[@id='servicecreate']/div[2]/div/div/div[2]/div[2]/div/div/form/div[4]/md-checkbox/div[2]/span[1]";
+	String SERVICE_ONDEMAND_BOOLEANBASECOST_PRICE= ".//*[@id='baseCostPrice']";
+	String SERVICE_ONDEMAND_BOOLEAN_BASEBILLABLE_PRICE= ".//*[@id='basebillablePrice']";
+	String SERVICE_DAILY_BASE_COST_PRICE= ".//*[@id='dailyCostPricev']";
+	String SERVICE_DAILY_BASE_BILLABLE_PRICE= ".//*[@id='dailyBillablePrice']";
+	String SERVICE_WEEKLY_BASE_COST_PRICE= ".//*[@id='weeklyCostPrice']";
+	String SERVICE_WEEKLY_BASE_BILLABLE_PRICE= ".//*[@id='weeklyBillablePrice']";
+	String SERVICE_MONTH_BASE_COST_PRICE= ".//*[@id='monthlyCostPrice']";
+	String SERVICE_MONTH_BASE_BILLABLE_PRICE= ".//*[@id='monthlyBillablePrice']";
+	/*Service Creation*/
+	
+	String SERVICE_BREAD_CRUMS=".//*[@id='page-wrapper']/div/ol";
+	/*Service */
+	
 
 }
