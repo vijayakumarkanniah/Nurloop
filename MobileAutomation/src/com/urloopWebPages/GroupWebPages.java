6218e300d42537cd768ab7084e054e8c61a0d4cc
ADMIN <ADMIN@TRINGAPPS>
2017-08-09 03:10:42 -0700

    Prabu Resolved

--- MobileAutomation/src/com/urloopWebPages/GroupWebPages.java
+++ MobileAutomation/src/com/urloopWebPages/GroupWebPages.java
@@ -1,5 +1,8 @@
 package com.urloopWebPages;
 
+import java.awt.AWTException;
+import java.io.FileNotFoundException;
+
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -28,30 +31,30 @@ public class GroupWebPages {
 	}
 
 	@Test(priority = 1)
-	public void login() throws InterruptedException {
+	public void login() throws InterruptedException, FileNotFoundException {
 		LoginWebPages loginWebPages = new LoginWebPages();
 		loginWebPages.configuration();
 		loginWebPages.validCredentials();
 	}
 
-	/*@Test(priority = 2)*/
-	public void createGroupsStaff() throws InterruptedException {
+	@Test(priority = 2)
+	public void createGroupsStaff() throws InterruptedException, AWTException {
 		groupGS.setGroupName(testDataSheet.getCellValue("ValidGroupStaff", "Groups", "GroupName"));
 		groupGS.setOnly_Staff(testDataSheet.getCellValue("ValidGroupStaff", "Groups", "Staff"));
 		groupGS.setDescription(testDataSheet.getCellValue("ValidGroupStaff", "Groups", "Description"));
 		groupWebScripts.createGroups(groupWeb_Object, groupGS);
 	}
 
-	/*@Test(priority = 3)*/
-	public void createGroupsResident() throws InterruptedException {
+	@Test(priority = 3)
+	public void createGroupsResident() throws InterruptedException, AWTException {
 		groupGS.setGroupName(testDataSheet.getCellValue("ValidGroupResident", "Groups", "GroupName"));
 		groupGS.setOnly_Residents(testDataSheet.getCellValue("ValidGroupResident", "Groups", "Resident"));
 		groupGS.setDescription(testDataSheet.getCellValue("ValidGroupResident", "Groups", "Description"));
 		groupWebScripts.createGroups(groupWeb_Object, groupGS);
 	}
 
-	/*@Test(priority = 4)*/
-	public void createGroupsEveryOne() throws InterruptedException {
+	@Test(priority = 4)
+	public void createGroupsEveryOne() throws InterruptedException, AWTException {
 		groupGS.setGroupName(testDataSheet.getCellValue("ValidGroupEveryOne", "Groups", "GroupName"));
 		groupGS.setEveryone(testDataSheet.getCellValue("ValidGroupEveryOne", "Groups", "Everyone"));
 		groupGS.setDescription(testDataSheet.getCellValue("ValidGroupEveryOne", "Groups", "Description"));
@@ -65,5 +68,10 @@ public class GroupWebPages {
 		groupGS.setDescription(testDataSheet.getCellValue("ValidGroupResident", "Groups", "Description"));
 		groupWebScripts.scroll(groupGS);
 	}
-
+     
+	@Test(priority = 6)
+	public void feedPosting() throws InterruptedException, AWTException{
+		groupGS.setFeedPost(testDataSheet.getCellValue("FeedPosting", "Groups", "FeedDetails"));
+		groupWebScripts.feedPosting(groupGS);
+	}
 }
